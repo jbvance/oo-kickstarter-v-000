c87635bd8b4b4f063aebf54d82903b8be7850cd1
Jason Vance <jbvance@gmail.com>
2017-11-29 14:10:46 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
   end
 
   def back_project(project)
-    if !@backed_projects.include?(projet)
+    if !@backed_projects.include?(project)
       @backed_projects.push(project)
       project.add_backer(self)
     end
